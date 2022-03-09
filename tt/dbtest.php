043366ef697f917ca11ece3e56dd4095ad0f5ac7
dollarstir <kpin463@gmail.com>
2022-03-09 09:05:17 +0000

    done

---
+++ tt/dbtest.php
@@ -0,0 +1,9 @@
+
+<?php
+require '../loader/autoloader.php';
+// $checker = new Session();
+$counter = new Counter();
+// $checker->mainchecker('/tms/login');
+ echo $counter->countall('eventb');  
+
+?>
