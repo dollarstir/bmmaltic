4b86389307ace385345dd5621356e231dde0cdd2
dollarstir <kpin463@gmail.com>
2022-03-15 22:18:05 +0000

    chnaged

--- engine.php
+++ engine.php
@@ -4,23 +4,23 @@ require_once 'core/router.php';
 require_once 'loader/viewer.php';
 $router = new Router();
 
-$router->route('bmm/', function () {
+$router->route('/', function () {
     return view('main.php');
 });
 
-$router->route('bmm/home', function () {
+$router->route('/home', function () {
     return view('main.php');
 });
 
-$router->route('bmm/services', function () {
+$router->route('/services', function () {
     return view('service.php');
 });
 
-$router->route('bmm/aboutus', function () {
+$router->route('/aboutus', function () {
     return view('about.php');
 });
 
-$router->route('bmm/contactus', function () {
+$router->route('/contactus', function () {
     return view('contact.php');
 });
 
