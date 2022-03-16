858c616899cd0efc6715d39c9725fc084268037a
dollarstir <kpin463@gmail.com>
2022-03-16 14:59:51 +0000

    check

--- processor.php
+++ processor.php
@@ -1,30 +1,31 @@
 <?php
+
 // Developed by: Frederick Ennin(CEO of Dollarsoft)
-//Email : kpin463@gmail.com 
+//Email : kpin463@gmail.com
 require 'loader/autoloader.php';
 
-if(isset($_GET['action'])){
+if (isset($_GET['action'])) {
     $action = $_GET['action'];
 
-    switch($action){
-        case "send":
+    switch ($action) {
+        case 'send':
             extract($_POST);
             $mail = new Mail();
 
-            $mail->sendmail('localhost', "Message From  BMM Maltic Website", $message, array('kpin463@gmail.com'), $email, $name);
+            $mail->sendmail('tuceehub.org', 'Message From  BMM Maltic Website', $message, ['kpin463@gmail.com'], $email, $name);
             break;
 
-        case "request":
+        case 'request':
             extract($_POST);
             $mail = new Mail();
 
-            $mail->sendmail('localhost', "Request From  BMM Maltic Website", $subject.'. Contact : '.$phone, array('kpin463@gmail.com'), $email, $name);
+            $mail->sendmail('tuceehub.org', 'Request From  BMM Maltic Website', $subject.'. Contact : '.$phone, ['kpin463@gmail.com'], $email, $name);
             break;
         case 'newsletter':
             extract($_POST);
             $mail = new Mail();
 
-            $mail->sendmail('localhost', "Newsletter From  BMM Maltic Website", $email, array('kpin463@gmail.com'), $email, $name);
+            $mail->sendmail('tuceehub.org', 'Newsletter From  BMM Maltic Website', $email, ['kpin463@gmail.com'], $email, $name);
             break;
     }
 }
