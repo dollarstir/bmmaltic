71ab5a1a05ded8b3bb8df48e8af8481131e89382
Frederick Ennin <kpin463@gmail.com>
2023-11-29 23:29:04 +0800

    d

--- engine.php
+++ engine.php
@@ -1,35 +1,3 @@
-<?php
-
-require_once 'core/router.php';
-require_once 'loader/viewer.php';
-$router = new Router();
-
-$router->route('/', function () {
-    return view('main.php');
-});
-
-$router->route('/home', function () {
-    return view('main.php');
-});
-
-$router->route('/services', function () {
-    return view('service.php');
-});
-
-$router->route('/aboutus', function () {
-    return view('about.php');
-});
-
-$router->route('/contactus', function () {
-    return view('contact.php');
-});
-
-$router->route('/sitemap', function () {
-    return view('sitemap.xml');
-});
-
-$router->launch($router->action());
-
 
 
 <?php
